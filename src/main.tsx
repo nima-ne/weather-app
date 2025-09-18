599658b7bce7dfc2fdc61017253c53077e5af8a6
unknown <nimaneshat44@gmail.com>
2025-09-18 14:52:32 +0430

    main type

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById('root') as any).render(
 
     <App />
 
